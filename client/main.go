7c8ca8e635695686ac04026c00cb454d15ee9d58
Kyaw Myint Thein <kyaw.myintthein@razer.com>
2021-07-29 13:50:53 +0800

    Add large object rpc client

--- client/main.go
+++ client/main.go
@@ -5,34 +5,16 @@ import (
 	"log"
 	"net/http"
 
-	userpbs "github.com/kyawmyintthein/twirp-poc/protopbs/protos/user/v1"
+	objectpbs "github.com/kyawmyintthein/twirp-poc/protopbs/protos/objects"
 	"github.com/twitchtv/twirp"
 )
 
 func main() {
 
-	client := userpbs.NewUserServceJSONClient("http://localhost:8080", &http.Client{}, twirp.WithClientPathPrefix("/rz"))
-	for i := 0; i < 5; i++ {
-		loginSession, err := client.Onboarding(context.Background(), &userpbs.UserOnboardingRequest{})
-		if err != nil {
-			// We got an error. Is it a twirp Error?
-			if twerr, ok := err.(twirp.Error); ok {
-				// Twirp errors support custom, arbitrary metadata. For example, a
-				// server could inform a client that a particular error is retryable.
-				if twerr.Meta("retryable") != "" {
-					log.Printf("got error %q, retrying", twerr)
-					continue
-				}
-			}
-			log.Fatal(err)
-		} else {
-			log.Println("onboarding response", loginSession)
-			break
-		}
-	}
+	client := objectpbs.NewObjectServiceProtobufClient("http://localhost:8080", &http.Client{}, twirp.WithClientPathPrefix("/rz"))
 
 	for i := 0; i < 5; i++ {
-		empty, err := client.UpdateProfile(context.Background(), &userpbs.UpdateProfileRequest{})
+		empty, err := client.GetObject(context.Background(), &objectpbs.ObjectRequest{})
 		if err != nil {
 			// We got an error. Is it a twirp Error?
 			if twerr, ok := err.(twirp.Error); ok {
